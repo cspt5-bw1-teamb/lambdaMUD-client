6d0780b393412d8859b56873e590387edc95f74e
Ken Ruf <ruf.kenichi@gmail.com>
2020-02-27 12:02:09 -0600

    successfully connected to test server

--- wander/src/components/Map.js
+++ wander/src/components/Map.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Map = () => {
+
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+
+        axios.get('https://lambda-mud-test.herokuapp.com/api/adv/rooms/', {headers: {'Authorization': `Token 013ba7e8edbbe9e2623416ea939ed403bfa2cef0`}})
+            .then(res => setRooms(res.data))
+            .catch(err => console.log(err))
+        }
+    )
+
     return (
-        <div>
+        <div className="map">
             <p>Map</p>
         </div>
     )
