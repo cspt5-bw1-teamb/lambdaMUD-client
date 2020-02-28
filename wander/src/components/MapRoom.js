be3061c9210a38900369b5dc6354e558936344f6
Ken Ruf <ruf.kenichi@gmail.com>
2020-02-27 21:56:04 -0600

    added styled components and switched MapRoom styling

--- wander/src/components/MapRoom.js
+++ wander/src/components/MapRoom.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+    border: 1px black solid;
+    width: 50px;
+    height: 50px;
+    background-color: ${props => props.roomId == props.currentRoomId ? "aquamarine" : "white"}
+`
 
 const MapRoom = props => {
     return (
-        <div className = {props.room.pk == props.currentRoomId ? "mapRoom currentRoom": "mapRoom"}>
+        <Box currentRoomId = {props.currentRoomId} roomId = {props.room.pk}>
             {props.room.pk}
-        </div>
+        </Box>
     )
 }
 
